45ca324de521f94abb2e75e9f7186aaa761375cd
Manga08 <bijuu5445@gmail.com>
2020-08-17 14:08:17 -0500

    [Featured]: The 0 Task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
 #Web servers preparation script.
-sudo apt-get -y update
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-sudo touch /data/web_static/releases/test/index.html
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Wenas
-  </body>
-</html>" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
-sudo service nginx restart
+apt-get -y update
+apt-get -y install nginx
+ufw allow 'Nginx HTTP'
+mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "I'm at MAGFest 2020" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data
+sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+service nginx restart
