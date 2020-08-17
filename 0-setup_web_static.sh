d9b94208f5c8b8f913d04d74e4ed45d72f8c4e13
Manga08 <bijuu5445@gmail.com>
2020-08-17 13:57:33 -0500

    [Featured]: The 0 Task

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,7 +2,7 @@
 #Web servers preparation script.
 sudo apt-get -y update
 sudo apt-get -y install nginx
-mkdir -p /data/web_static/releases/test /data/web_static/shared
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 sudo touch /data/web_static/releases/test/index.html
 echo "<html>
   <head>
@@ -13,5 +13,5 @@ echo "<html>
 </html>" > /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server;/a location /hbnb_static/ { alias /data/web_static/current/; }' /etc/nginx/sites-available/default
 sudo service nginx restart
