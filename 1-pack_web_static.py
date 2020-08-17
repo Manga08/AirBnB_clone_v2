f042a0cf16186c79a685e685fa84e3078dc91fcd
Manga08 <bijuu5445@gmail.com>
2020-08-17 14:43:49 -0500

    [Featured]: The 1 Task

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,10 +7,10 @@ from fabric.operations import local
 def do_pack():
     """Function to generate version compressed files"""
     local("mkdir -p versions")
-    file_name = "web_static_{}.tgz".format(
+    Name_Fl = "web_static_{}.tgz".format(
         datetime.strftime(datetime.now(), "%Y%m%d%H%M%S"))
-    archive = local("tar -zcvf versions/{} web_static".format(file_name))
+    archive = local("tar -zcvf versions/{} web_static".format(Name_Fl))
 
     if archive.failed:
         return None
-    return file_name
+    return Name_Fl
