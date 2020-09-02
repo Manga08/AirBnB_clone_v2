3f672168aae8d765cc91c8541d122a336cc20e2e
Manga08 <bijuu5445@gmail.com>
2020-09-02 18:08:50 -0500

    [Featured]: The 8 Task

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -8,14 +8,14 @@ app = Flask(__name__, template_folder='templates')
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    """ Display HTML page with list of states """
+    """Display HTML page with list of states."""
     return render_template('7-states_list.html',
                            states=storage.all(State).values())
 
 
 @app.teardown_appcontext
 def remove_session(response_or_exc):
-    """ Remove the current SQLAlchemy session """
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
